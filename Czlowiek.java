e2cd90067e294926051d221c68bb1c0fb8199cb2
KubaKesek <118059110+KubaKesek@users.noreply.github.com>
2022-11-12 19:16:51 +0100

    Add files via upload
    Do skończenia

---
+++ Czlowiek.java
@@ -0,0 +1,44 @@
+package zajecia;
+
+public class Czlowiek {
+
+
+    private int życie;
+    private int exp;
+    private int obrazenia;
+
+    public int getŻycie() {
+        return życie;
+    }
+
+    public void setŻycie(int życie) {
+        this.życie = życie;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public int getObrazenia() {
+        return obrazenia;
+    }
+
+    public void setObrazenia(int obrazenia) {
+        this.obrazenia = obrazenia;
+    }
+
+
+    public Czlowiek() {
+        this.exp = 100;
+        this.życie = 100;
+        this.obrazenia = 50;
+
+
+    }
+    public void statystyki
+    }
+
---
+++ Main.java
@@ -0,0 +1,30 @@
+package zajecia;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main  {
+ public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);
+   Czlowiek gracz = new Czlowiek();
+   while(true) {
+       String wybrany_tryb;
+
+       System.out.println("Opcje:");
+       System.out.println("Wyjście    Walka    Wyświetl Statystyki   Leczenie");
+       System.out.println("Wybierz tryb gry");
+       System.out.println();
+
+
+   }
+
+
+
+
+
+
+
+
+
+ }
+}
